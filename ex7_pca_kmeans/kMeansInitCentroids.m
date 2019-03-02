a145273ea5b8c20d9c4d9e434729d9aeb9266b6f
smivv <smivvla@gmail.com>
2019-03-02 22:48:49 +0300

    Added K-Means and PCA algorithms

---
+++ ex7_pca_kmeans/kMeansInitCentroids.m
@@ -0,0 +1,29 @@
+function centroids = kMeansInitCentroids(X, K)
+%KMEANSINITCENTROIDS This function initializes K centroids that are to be 
+%used in K-Means on the dataset X
+%   centroids = KMEANSINITCENTROIDS(X, K) returns K initial centroids to be
+%   used with the K-Means on the dataset X
+%
+
+% You should return this values correctly
+centroids = zeros(K, size(X, 2));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: You should set centroids to randomly chosen examples from
+%               the dataset X
+%
+
+% get the randomly-selected indices
+indices = randperm(length(X));
+indices = indices(1:K);
+% choose the subset of a you want
+centroids = X(indices, :);
+
+
+
+
+
+% =============================================================
+
+end
+
